19cb4a49828432c121801b2de824310fec812922
felipe931 <felipe.manoel.lima.zatte@gmail.com>
2025-10-11 12:05:14 -0300

    aula 11

--- quests-to-doo-do/src/App.js
+++ quests-to-doo-do/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Flex h-screen w-screen itemss-cventer justify-center">
+       <div> className="card w-[80%] lg:w-[50%] h-[70%] shadow-md rouded-sm transform ease-out duration-300 items-center  p-10 gap-5"
+       </div>
     </div>
   );
 }
