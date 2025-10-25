ad89978bbee435fbc206a645d674e79262a653a5
felipe931 <felipe.manoel.lima.zatte@gmail.com>
2025-10-25 12:30:08 -0300

    02

--- quests-to-doo-do/src/App.js
+++ quests-to-doo-do/src/App.js
@@ -1,7 +1,31 @@
+
 import AddQuest from "./ADDQuest";
+import { useState } from "./ADDQuest";
+import Questlist from "./Quest.lits";
+
+const localStorageQuests = JSON.parse(window.localStorage.getItem("quests")) || [];
+const [quests, setQuests] = useState(localStorageQuests);
 
 function App() {
-  function saveADDQuest(title){console.log(title)}
+  const [quests, setQuests] = useState({});
+  function saveADDQuest(title){let auQuests= perguntas;
+    let id = 0
+    if (Object.length){id = auxQuests[auxQuests.length - 1].id} id++;
+
+    const createdQuest = {
+      id:id,
+      title:title,
+      status:"aberto",
+      created_at:new Date(Date.now()).toUTCString(),
+    } }
+   auQuests.push(createdQuest);
+    localStorage.setItem("quests", JSON.stringify(auQuests));
+    getQuests();
+  
+    function getQuests() {
+      setQuests(JSON.parse(window.localStorage.getItem("quests")));
+    }
+  
   return (
     <div className="Flex h-screen w-screen itemss-cventer justify-center">
        <div className="card w-[80%] lg:w-[50%] h-[70%] shadow-md rouded-sm transform ease-out duration-300 items-center  p-10 gap-5">
@@ -9,6 +33,7 @@ function App() {
           Quest To do 
         </h1>
         <AddQuest saveADDQuest={saveADDQuest}/>
+        <questlist queests={quest}/>
        </div>
     </div>
   );
---
+++ quests-to-doo-do/src/Quest.lits.js
@@ -0,0 +1,10 @@
+export default function questList(props) {
+    return (
+     <div className="flex flex-col overflow-y-auto gap-6 w-[80%]">
+        {props.quests.map((quest) => {
+            return <p key={quest.id}>{quest.title}
+            </p>;
+        })}
+     </div>
+    )
+}
